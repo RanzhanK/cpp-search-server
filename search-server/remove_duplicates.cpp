3865130a8b7ca343afeeb2bcaf3dc33f50fa054a
RanzhanK <ranzhan@yandex.ru>
2023-02-27 23:02:36 +0300

    Change RemoveDuplicates method

--- search-server/remove_duplicates.cpp
+++ search-server/remove_duplicates.cpp
@@ -17,27 +17,26 @@
 
 //метод поиска и удаления дубликатов
 void RemoveDuplicates(SearchServer &search_server) {
-    std::vector<std::pair<int, std::vector<std::string>>> document_struct;
+    LOG_DURATION("RemoveDuplicates", std::cerr);
 
-    for (const int document_id: search_server.GetDocumentId()) {
+    std::set <std::set<std::string>> document_struct;
+    std::set<int> duplicates;
+
+    for (const int document_id: search_server) {
+        std::set <std::string> key_words;
         std::map<std::string, double> words_freq_id = search_server.GetWordFrequencies(document_id);
-        std::vector<std::string> key_words;
-
-        for (std::map<std::string, double>::iterator i = words_freq_id.begin(); i != words_freq_id.end(); ++i) {
-            key_words.push_back(i->first);
+        for (auto &&[first, second]: words_freq_id) {
+            key_words.insert(first);
         }
-
-        document_struct.push_back(std::make_pair(document_id, key_words));
+        if (document_struct.count(key_words) == 0) {
+            document_struct.insert(key_words);
+        } else {
+            std::cout << "Found duplicate document id " << document_id << std::endl;
+            duplicates.insert(document_id);
+        }
     }
 
-    for (auto i = 0; i < (int) document_struct.size() - 1; ++i) {
-        for (auto j = i + 1; j < (int) document_struct.size(); ++j) {
-            if (document_struct[i].second == document_struct[j].second) {
-                std::cout << "Found duplicate document id " << document_struct[j].first << std::endl;
-                search_server.RemoveDocument(document_struct[j].first);
-                document_struct.erase(document_struct.begin() + j);
-                j--;
-            }
-        }
+    for (auto document_id: duplicates) {
+        search_server.RemoveDocument(document_id);
     }
 }
